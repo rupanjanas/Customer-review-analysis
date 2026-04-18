5c3f91fb7c1810938a348fc7ea0e194a2144af36
rupanjanas <saharupanjana04@gmail.com>
2026-04-18 18:06:20 +0530

    Update Segmentation.jsx

--- ai-review-intelligence/src/pages/Segmentation.jsx
+++ ai-review-intelligence/src/pages/Segmentation.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Bar, Bubble, Pie } from "react-chartjs-2";
+import { Bar, Pie } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   LinearScale,
